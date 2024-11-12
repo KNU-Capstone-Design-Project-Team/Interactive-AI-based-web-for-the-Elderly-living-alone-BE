9b2d0ce3303acc2372a3af3eccad26f23286b061
chaechaekim <chaeeun7478@naver.com>
2024-11-12 21:23:25 +0900

    feat: create guardian code

--- app/models/joinMembershipModels.py
+++ app/models/joinMembershipModels.py
@@ -2,12 +2,30 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from pymongo import MongoClient
-
+import random
+import string
 
 load_dotenv()
 client = MongoClient(os.getenv("MONGO_URI"))
+db = client.ElderCareNet # 사용할 데이터베이스
+guardians = db.guardians  # 보호자 정보를 저장할 컬렉션
 
+#6자리 보호자의 고유 코드를 생성하고 DB에 저장하는 함수!
 def produceConnectionNum():
-    pass
 
+    # 6자리의 보호자 코드 생성 (영문 대문자와 숫자로 구성)
+    connection_num = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
 
+    # 현재 시간을 기준으로 보호자 코드 데이터 생성
+    guardian_data = {
+        "connection_num": connection_num,
+        "created_at": datetime.now()  # 코드 생성 시간
+    }
+    #return connection_num -> test
+
+    # MongoDB에 데이터 삽입
+    guardians.insert_one(guardian_data)
+    return connection_num  # 생성한 보호자 코드를 반환
+
+#test= produceConnectionNum()
+#print(test)
